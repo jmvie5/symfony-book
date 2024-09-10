b76a013e01ec3bb2fbde61a863435b3d48900144
Jean-Michel Viel <jeanmichel.viel@gmail.com>
2024-09-10 10:19:15 -0400

    add spam filter with akismet

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,12 +7,14 @@ use App\Entity\Conference;
 use App\Form\CommentType;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use App\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 
 class ConferenceController extends AbstractController
 {
@@ -34,6 +36,7 @@ class ConferenceController extends AbstractController
         Request $request, 
         Conference $conference, 
         CommentRepository $commentRepository,
+        SpamChecker $spamChecker,
         #[Autowire('%photo_dir%')] string $photoDir,
     ): Response {
         $comment = new Comment();
@@ -48,6 +51,18 @@ class ConferenceController extends AbstractController
             }
             
             $this->entityManager->persist($comment);
+
+            $context = [
+                'user_ip' => $request->getClientIp(),
+                'user_agent' => $request->headers->get('user-agent'),
+                'referrer' => $request->headers->get('referer'),
+                'permalink' => $request->getUri(),
+            ];
+
+            if (2 === $spamChecker->getSpamScore($comment, $context)) {
+                throw new \RuntimeException("Va t'en le spam!");
+            }
+
             $this->entityManager->flush();
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
